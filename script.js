66e7ca6732f0f91cc0b6d982df09b3ce47cd699c
Carrington <hartis.c99@gmail.com>
2022-03-04 17:14:59 -0500

    past, present, and future change based on current time

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 var textElements = document.querySelectorAll(".text-input");
 var saveButtons = document.querySelectorAll(".saveBtn");
-console.log(textElements);
+var today = moment();
+var currentHour = moment().hour();
 
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+textElements.forEach((input) => {
+  if (input.id < currentHour) {
+    input.style.backgroundColor = "gray";
+    console.log(input.id);
+  } else if (input.id > currentHour) {
+    input.style.backgroundColor = "green";
+  } else {
+    input.style.backgroundColor = "red";
+  }
+});
+
+// Local storage.
 saveButtons.forEach((saveButton, i) => {
   saveButton.addEventListener("click", (event) => {
     event.preventDefault();
-    var saveText = Array.from(textElements, (text) => text.value);
-    localStorage.setItem("Save-Event", JSON.stringify(saveText));
+    var storeText = Array.from(textElements, (text) => text.value);
+
+    // Grab single text value.
+    var singleTextArea;
+    textElements.forEach((textArea) => {
+      if (textArea.id === event.target.name) {
+        singleTextArea = textArea;
+      }
+    });
+
+    localStorage.setItem("Save-Event", JSON.stringify(storeText));
   });
   textElements[i].value = JSON.parse(localStorage.getItem("Save-Event"))[i];
 });
